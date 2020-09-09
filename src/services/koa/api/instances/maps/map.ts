2f14c4dec6722772a2f6e0f592a60f7441fec8bb
multivit4min <david.kartnaller@gmail.com>
2020-09-09 18:26:11 +0000

    implements #1

--- src/services/koa/api/instances/maps/map.ts
+++ src/services/koa/api/instances/maps/map.ts
@@ -8,6 +8,32 @@ api.get("/", async ctx => {
   ctx.body = ctx.state.map!
 })
 
+api.patch("/position/:toIndex", perm(MapScope.MANAGE), async ctx => {
+  const { index, map, mode, rounds } = ctx.state.map!
+  const toIndex = parseInt(ctx.request.params.toIndex, 10)
+  if (isNaN(toIndex)) {
+    ctx.status = 400
+    return ctx.body = { message: "invalid index given, expected a number" }
+  }
+  //maximum allowed map index
+  const maximum =  ctx.state.instance!.state.get("maps").length + 1
+  if (toIndex < 0 || toIndex > maximum) {
+    ctx.status = 400
+    return ctx.body = { message: `invalid index given, expected a number within 0-${maximum}` }
+  }
+  const { battlefield } = ctx.state.instance!
+  try {
+    console.log({ index, toIndex, map })
+    await battlefield.delMap(index)
+    await battlefield.addMap(map, mode, rounds, toIndex)
+    await ctx.state.instance!.mapList()
+    ctx.status = 200
+  } catch (e) {
+    ctx.status = 500
+    ctx.body = { message: e.message }
+  }
+})
+
 api.post("/next", perm(MapScope.SWITCH), async ctx => {
   try {
     const { instance } = ctx.state
