ab9e7ad72541f0ef528cbd7fc0aa4848b5458e6e
multivit4min <david.kartnaller@gmail.com>
2020-09-22 17:40:44 +0000

    fix log scope

--- src/services/koa/api/instances/logs/index.ts
+++ src/services/koa/api/instances/logs/index.ts
@@ -1,7 +1,7 @@
 import Router from "koa-joi-router"
 import { LogMessage } from "@entity/LogMessage"
 import { perm } from "@service/koa/permission"
-import { InstanceScope, PluginScope } from "@service/permissions/Scopes"
+import { InstanceScope } from "@service/permissions/Scopes"
 
 const api = Router()
 
@@ -12,14 +12,14 @@ api.get("/instance", perm(InstanceScope.LOGS), async ctx => {
   )).map(e => e.toJSON())
 })
 
-api.get("/plugins", perm(PluginScope.LOGS), async ctx => {
+api.get("/plugins", perm(InstanceScope.LOGS), async ctx => {
   ctx.body = (await LogMessage.getMessages(
     ctx.state.instance!.id,
     { source: LogMessage.Source.PLUGIN }
   )).map(e => e.toJSON())
 })
 
-api.get("/plugins/:pluginName", perm(PluginScope.LOGS), async ctx => {
+api.get("/plugins/:pluginName", perm(InstanceScope.LOGS), async ctx => {
   ctx.body = (await LogMessage.getMessages(
     ctx.state.instance!.id,
     { source: LogMessage.Source.PLUGIN, sourceLocation: ctx.request.params.pluginName }
