086118d6ac6a7deea5545ca4c25bfb1a625f8374
David Kartnaller <david.kartnaller@gmail.com>
2020-10-17 12:08:13 +0200

    slightly update player id fetching

--- src/services/orm/entity/Player.ts
+++ src/services/orm/entity/Player.ts
@@ -3,6 +3,7 @@ import { AbstractEntity } from "./Abstract"
 import { ChatMessage } from "./ChatMessage"
 import { Kill } from "./Kill"
 import { Battlefield } from "vu-rcon"
+import winston from "winston"
 
 @Entity()
 export class Player extends AbstractEntity<Player> {
@@ -33,48 +34,49 @@ export class Player extends AbstractEntity<Player> {
   }
 
   /** gets a list of ids by the name */
-  static async findIdsByName(names: string[]): Promise<Record<string, number|undefined>> {
-    const entities = await Player
+  static async findIdsByName(names: string[]): Promise<Player[]> {
+    return Player
       .createQueryBuilder()
       .select("id, name")
       .where({ name: In(names) })
       .getRawMany()
-    return Object.fromEntries(names.map(name => {
-      const e = entities.find(e => e.name === name)
-      return [name, e ? e.id : undefined]
-    }))
   }
 
   /** retrieves multiple player ids by their name and indexes them by an identifier */
-  static async getPlayerIds<
-    T extends Record<string, number|undefined>
-  >(
+  static async getPlayerIds(
     names: Record<string, string|undefined>,
     retrieve: (name: string) => Promise<Battlefield.Player|undefined>
-  ): Promise<T> {
-    const result = Object.fromEntries(Object.keys(names).map(id => [id, undefined])) as T
+  ): Promise<Record<string, number|undefined>> {
+    //create the initial response data
+    const result: Record<string, number|undefined> = Object.fromEntries(Object.keys(names).map(id => [id, undefined]))
+    //retrieve all names which should be retrieves
     const filtered = Object.keys(names)
       .filter(id => typeof names[id] === "string")
-      .reduce((acc, id) => acc.includes(names[id] as string) ? acc : [...acc, names[id] as string], [] as string[])
-    const found = await Player.findIdsByName(filtered)
-    const inserts = Object.keys(names)
-      .filter(name => !found[name] && filtered.includes(name))
+      //retrieves all names which should be fetched from database
+      .reduce((acc, id) => acc.includes(names[id]) ? acc : [...acc, names[id]], [] as (string|undefined)[]) as string[]
+    //gets a list of players in the database
+    const players = await Player.findIdsByName(filtered)
+    //gets a list of all players which have not been found in the database
+    const inserts = Object.keys(filtered)
+      //checks if a name has not been found
+      .filter(name => !players.some(p => p.name === name))
+      //do not insert duplicates
       .reduce((acc, name) => acc.includes(name) ? acc : [...acc, name], [] as string[])
-    if (inserts.length > 0) {
-      await Promise.all(inserts.map(async name => {
-        const data = await retrieve(name)
-        if (!data) return
-        //@ts-ignore
-        result[name] = (await Player.from(data)).id
-      }))
-    }
-    Object.keys(found).forEach(name => {
+    //insert all players which are not in our database
+    winston.verbose({ inserts })
+    await Promise.all(inserts.map(async name => {
+      const data = await retrieve(name)
+      if (!data) return winston.verbose(`could not find player with name "${name}" in retrieve`)
+      result[name] = (await Player.from(data)).id
+    }))
+    players.forEach(player => {
       Object.keys(names)
-        .filter(id => names[id] === name)
-        //@ts-ignore
-        .forEach(id => result[id] = found[name])
+        .filter(name => names[name] === player.name)
+        .forEach(name => result[name] = player.id)
     })
-    return result as T
+    return result
+
+
   }
 
 }
--- src/util/winston.ts
+++ src/util/winston.ts
@@ -48,7 +48,7 @@ winston.configure({
   ),
   transports: [
     new transports.Console({
-      level: "info",
+      level: "verbose",
       format: format.combine(
         format.timestamp({format: "DD.MM.YYYY HH:mm:ss.SSS"}),
         prettyLog(),
