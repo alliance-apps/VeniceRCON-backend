e3dd59c7f78f7eb8514b45059b1d5f6f08d2f9ca
multivit4min <david.kartnaller@gmail.com>
2020-10-28 11:42:28 +0000

    allow more variations of config schema

--- src/services/plugin/schema.ts
+++ src/services/plugin/schema.ts
@@ -1,19 +1,70 @@
 import { Joi } from "koa-joi-router"
 
+const baseVarSchema = Joi.object({
+  name: Joi.string().required(),
+  description: Joi.string().required()
+})
+
+/** allows simple strings */
+export const stringSchema = baseVarSchema.keys({
+  type: Joi.string().valid("string").required(),
+  default: Joi.string().default("").optional()
+})
+
+/** allows integers and floats */
+export const numberSchema = baseVarSchema.keys({
+  type: Joi.string().valid("number").required(),
+  default: Joi.number().default(0).optional()
+})
+
+/** allows true / false */
+export const booleanSchema = baseVarSchema.keys({
+  type: Joi.string().valid("boolean").required(),
+  default: Joi.boolean().default(false).optional()
+})
+
+/** allows an array of strings */
+export const stringsSchema = baseVarSchema.keys({
+  type: Joi.string().valid("strings").required(),
+  default: Joi.array().items(Joi.string()).default([]).optional()
+})
+
+/** allows multiple options */
+export const selectSchema = baseVarSchema.keys({
+  type: Joi.string().valid("select").required(),
+  options: Joi.array().items(Joi.string()).required(),
+  default: Joi.array().items(Joi.string()).default("").optional()
+})
+
+const baseSchemas = [
+  stringSchema,
+  numberSchema,
+  booleanSchema,
+  stringsSchema,
+  selectSchema
+]
+
+
+/** allows an array of strings */
+export const arraySchema = baseVarSchema.keys({
+  type: Joi.string().valid("array").required(),
+  vars: Joi.array().items(...baseSchemas).required()
+})
+
+/** create schema early so it can be required a circular dependency */
+export const varSchema = Joi.array()
+  .items(...baseSchemas, arraySchema)
+  .default([])
+  .optional()
+
 export const metaSchema = Joi.object({
   name: Joi.string().min(3).required(),
   description: Joi.string().optional(),
   version: Joi.string().regex(/^\d+\.\d+\.\d+$/).required(),
-  backend: Joi.string().valid("VU", "BF3", "*").required(),
+  backend: Joi.string().valid("VU", "BF3", "*").insensitive().uppercase().required(),
   entry: Joi.string().required(),
-  dependency: Joi.array().items(Joi.string()).optional(),
-  features: Joi.array().items(Joi.string().valid("router")).optional(),
-  vars: Joi.array().items(Joi.object({
-    name: Joi.string().required(),
-    description: Joi.string().required(),
-    type: Joi.string().valid("string", "number", "boolean").required(),
-    default: Joi.any().required()
-  })).optional()
+  dependency: Joi.array().items(Joi.string()).optional().default([]),
+  vars: varSchema
 })
 
 export function checkVariableSchema(vars: PluginVariable[], data: Record<string, any>) {
@@ -26,11 +77,14 @@ function buildVariableSchema(vars: PluginVariable[]) {
   })))
 }
 
-function getVariableType(type: "string"|"number"|"boolean") {
+function getVariableType(type: "string"|"number"|"boolean"|"strings"|"array"|"select") {
   switch(type) {
     case "string": return Joi.string().optional()
     case "number": return Joi.number().optional()
     case "boolean": return Joi.boolean().optional()
+    case "strings": return Joi.array().items(Joi.string()).optional()
+    case "array": return Joi.array().optional()
+    case "select": return Joi.string().optional()
   }
 }
 
@@ -40,15 +94,17 @@ export interface Meta {
   version: string
   backend: "BF3"|"VU"|"*"
   entry: string
-  features?: ("router")[]
-  dependency?: string[]
-  vars?: PluginVariable[]
+  dependency: string[]
+  vars: PluginVariable[]
 }
 
 export type PluginVariable =
   PluginStringVariable |
   PluginNumberVariable |
-  PluginBooleanVariable
+  PluginBooleanVariable |
+  PluginStringsVariable |
+  PluginArrayVariable |
+  PluginSelectVariable
 
 export interface PluginBaseVariable {
   name: string
@@ -68,4 +124,20 @@ export interface PluginNumberVariable extends PluginBaseVariable {
 export interface PluginBooleanVariable extends PluginBaseVariable {
   type: "boolean"
   default: boolean
+}
+
+export interface PluginStringsVariable extends PluginBaseVariable {
+  type: "strings"
+  default: string[]
+}
+
+export interface PluginSelectVariable extends PluginBaseVariable {
+  type: "select"
+  options: string[]
+  default: string
+}
+
+export interface PluginArrayVariable extends PluginBaseVariable {
+  type: "array"
+  vars: PluginVariable[]
 }
---
+++ tests/plugins/metaSchema.spec.ts
@@ -0,0 +1,104 @@
+import { Meta, metaSchema, PluginVariable, varSchema } from "../../src/services/plugin/schema"
+
+const schema = (meta: Partial<Meta> = {}): Meta => ({
+  name: "example",
+  description: "description of example",
+  version: "1.0.0",
+  backend: "*",
+  entry: "index.js",
+  dependency: [],
+  vars: [],
+  ...meta
+})
+
+describe("MetaSchema", () => {
+
+  it("should validate a basic schema successfully", async () => {
+    expect.assertions(1)
+    const data = schema()
+    //@ts-ignore
+    delete data.dependency
+    //@ts-ignore
+    delete data.vars
+    expect(await metaSchema.validate(schema())).toEqual(schema())
+  })
+
+  describe("schema.backend", () => {
+    it("should validate case insensitive", async () => {
+      expect.assertions(1)
+      //@ts-expect-error
+      expect(await metaSchema.validate(schema({ backend: "vu" })))
+        .toEqual(schema({ backend: "VU" }))
+    })
+
+    it("should error on invalid value", async () => {
+      expect.assertions(1)
+      try {
+        //@ts-expect-error
+        await metaSchema.validate(schema({ backend: "invalid" }))
+      } catch (e) {
+        expect(e).toBeInstanceOf(Error)
+      }
+    })
+  })
+
+  describe("schema.vars", () => {
+
+    const vars = (type: any, data: Partial<PluginVariable> = {}): [PluginVariable] => ([{
+      name: "foo",
+      description: "description of foo",
+      type,
+      ...data
+    }] as [PluginVariable])
+
+    it("should validate a string schema", async () => {
+      expect.assertions(1)
+      expect(await varSchema.validate(vars("string")))
+        .toEqual(vars("string", { default: "" }))
+    })
+
+    it("should validate a number schema", async () => {
+      expect.assertions(1)
+      expect(await varSchema.validate(vars("number")))
+        .toEqual(vars("number", { default: 0 }))
+    })
+
+    it("should validate a boolean schema", async () => {
+      expect.assertions(1)
+      expect(await varSchema.validate(vars("boolean")))
+        .toEqual(vars("boolean", { default: false }))
+    })
+
+    it("should validate a strings schema", async () => {
+      expect.assertions(1)
+      expect(await varSchema.validate(vars("strings")))
+        .toEqual(vars("strings", { default: [] as any }))
+    })
+
+    it("should validate a select schema", async () => {
+      expect.assertions(1)
+      const schema = vars("select", { options: ["foo", "bar"] })
+      expect(await varSchema.validate(schema))
+        .toEqual(vars("select", { default: "", options: ["foo", "bar"] }))
+    })
+
+    it("should validate a nested array schema", async () => {
+      expect.assertions(1)
+      const schema = vars("array", {
+        vars: vars("string")
+      })
+      expect(await varSchema.validate(schema))
+        .toEqual([{
+          name: "foo",
+          description: "description of foo",
+          type: "array",
+          vars: [{
+            name: "foo",
+            description: "description of foo",
+            type: "string",
+            default: ""
+          }]
+        }])
+    })
+  })
+})
