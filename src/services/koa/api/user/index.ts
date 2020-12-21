a699d697e14baedb210a02cdc7a2eb3eba0f616c
multivit4min <david.kartnaller@gmail.com>
2020-12-21 15:42:01 +0000

    implement user creation via administration api

--- src/services/koa/api/user/index.ts
+++ src/services/koa/api/user/index.ts
@@ -1,11 +1,37 @@
 import { User } from "@entity/User"
+import { perm } from "@service/koa/permission"
+import { UserScope } from "@service/permissions/Scopes"
 import Router from "koa-joi-router"
 import userRouter from "./user"
 
 const api = Router()
+const { Joi } = Router
 
 api.get("/", async ctx => {
   ctx.body = await User.getUsersAndPermissions()
+})
+
+//creates a new user
+api.route({
+  method: "POST",
+  path: "/",
+  validate: {
+    type: "json",
+    body: Joi.object({
+      username: Joi.string().min(3).required(),
+      password: Joi.string().min(6).required()
+    }).required()
+  },
+  pre: perm(UserScope.CREATE),
+  handler: async ctx => {
+    const { username, password } = ctx.request.body
+    if (await User.findOne({ username })) {
+      ctx.body = { message: "username is already taken" }
+      return ctx.status = 400
+    }
+    const user = await User.from({ username, password })
+    ctx.body = await user.getUserAndPermissions()
+  }
 })
 
 api.param("userId", async (id, ctx, next) => {
