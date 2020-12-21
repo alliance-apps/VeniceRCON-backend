dee6bc8ed618842be63f9ccd6f5f15e906d8fb9f
multivit4min <david.kartnaller@gmail.com>
2020-12-21 11:41:14 +0000

    allow custom configs

--- src/services/config/index.ts
+++ src/services/config/index.ts
@@ -7,9 +7,14 @@ import { updateLogLevel } from "../../util/winston"
 
 export let config: Configuration
 
-export async function initialize() {
+export async function initialize(args: Record<string, string>) {
   const basepath = path.join(__dirname, "/../../../")
-  const data = yaml.parse(await fs.readFile(`${basepath}/config.yaml`, "utf-8"))
+  let data: any
+  if (typeof args["--config"] === "string") {
+    data = yaml.parse(await fs.readFile(`${basepath}/${args["--config"]}`, "utf-8"))
+  } else {
+    data = yaml.parse(await fs.readFile(`${basepath}/config.yaml`, "utf-8"))
+  }
   try {
     config = {
       ...await schema.validateAsync(data, { allowUnknown: true }),
